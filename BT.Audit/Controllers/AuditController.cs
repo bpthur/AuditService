3d109aa19f4235d1deaf9307c32c1c7bf2852766
unknown <ben.thurecht@gmail.com>
2015-10-20 19:35:59 -0400

    updates

---
+++ BT.Audit/Controllers/AuditController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using BT.Audit.DomainModel;
+
+namespace AuditService.Controllers
+{
+    public class AuditController : ApiController
+    {
+		/// <summary>
+		/// Search Audits
+		/// </summary>
+		/// <param name="category"></param>
+		/// <returns></returns>
+		[HttpGet]
+	    public List<Audit> SearchAudits(string category)
+	    {
+			//TODO: Implement
+		    return new List<Audit>(){ new Audit("Test")};
+	    }
+    }
+}
---
+++ BT.Audit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuditService.WebApiApplication" Language="C#" %>
