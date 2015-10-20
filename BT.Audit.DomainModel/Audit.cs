2e2ac9bb6291d00a2d64d86eeb84e8931c375ef6
unknown <ben.thurecht@gmail.com>
2015-10-19 23:01:53 -0400

    updates

--- BT.Audit.DomainModel/Audit.cs
+++ BT.Audit.DomainModel/Audit.cs
@@ -2,8 +2,15 @@
 
 namespace BT.Audit.DomainModel
 {
+	/// <summary>
+	/// Representation of an Auditable Event
+	/// </summary>
     public class Audit
     {
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="category"></param>
 	    public Audit(string category)
 	    {
 		    Timestamp = DateTime.UtcNow;
